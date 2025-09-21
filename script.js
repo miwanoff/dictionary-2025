44e860342b233770b669b81477bbe2f56403bec9
miwanoff <marina.iva.noffa@gmail.com>
2025-09-21 13:04:41 +0300

    Change to  enUa

--- script.js
+++ script.js
@@ -4,19 +4,30 @@ const words = {
   apple: "яблуко",
 };
 
-for (const key in words) {
-  const element = words[key];
-  alert(element);
-  const word = prompt("Input translation");
-  if (word === key) {
-    alert("Correct!");
-  } else {
-    alert("Error!");
+const lang = prompt(
+  "Введіть enUa або uaEn \nпереклад з англійської на українську або з української на англійську"
+);
+
+const n = prompt("Input a number");
+
+label1: for (let i = 0; i < n; i++) {
+  for (const key in words) {
+    const element = key;
+    alert(element);
+    const word = prompt("Input translation or !");
+    if (word === "!") {
+      alert("Bye!");
+      break label1;
+    }
+    if (word === words[key]) {
+      alert("Correct!");
+    } else {
+      alert("Error!");
+    }
   }
 }
 
-
 for (const key in words) {
   const element = words[key];
-  document.writeln(element);
+  document.writeln(`${key}: ${element} <br>`);
 }
